8f69938af06b9349628fd59ab42e00c47131f705
thramousse <theo.ramousse@etu.uca.fr>
2022-01-12 17:32:34 +0100

    Initialized api project

---
+++ Sources/GraphQLApi/Profiles/AutoMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Model;
+using TarotDTO;
+
+namespace GraphQLApi.Profiles
+{
+    public class AutoMappingProfile : Profile
+    {
+        public AutoMappingProfile()
+        {
+            CreateMap<Game, GameDTO>(); // means you want to map from User to UserDTO
+            CreateMap<Player, PlayerDTO>();
+        }
+    }
+}
---
+++ Sources/TarotDTO/GameDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarotDTO
+{
+    public class GameDTO
+    {
+        public long Id { get; private set; }
+
+        public DateTime Date { get; private set; }
+
+        public int TakerPoints { get; private set; }
+
+        public bool? Excuse { get; private set; }
+
+        public bool? TwentyOne { get; private set; }
+    }
+}
