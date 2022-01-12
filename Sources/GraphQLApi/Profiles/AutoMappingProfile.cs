d689564101298bcf9c37643913868257d6873eb4
thramousse <theo.ramousse@etu.uca.fr>
2022-01-12 19:54:23 +0000

    Ajout fichiers api graphql

---
+++ Sources/GraphQLApi/Profiles/AutoMappingProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Model;
+using System;
+using System.Collections.Generic;
+using TarotDTO;
+
+namespace GraphQLApi.Profiles
+{
+    public class AutoMappingProfile : Profile
+    {
+        public AutoMappingProfile()
+        {
+            CreateMap<Game, GameDTO>(); // means you want to map from User to UserDTO
+            CreateMap<KeyValuePair<Player, Bidding>, PlayerAndBiddingDTO>().ConstructUsing(v => new PlayerAndBiddingDTO()
+            {
+                Player = new PlayerDTO()
+                {
+                    Id = v.Key.Id,
+                    LastName = v.Key.LastName,
+                    FirstName = v.Key.FirstName,
+                    NickName = v.Key.NickName
+                },
+                Bidding = v.Value
+            });
+        }
+    }
+}
