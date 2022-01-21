90b762d4b4c850e24e2217f6fca4449e4a60eea8
maalbiero <mathieu.albiero@etu.uca.fr>
2022-01-21 09:54:07 +0100

    Avancée de l'implémentation de GraphQL

--- Sources/GraphQLApi/Queries/GameQuery.cs
+++ Sources/GraphQLApi/Queries/GameQuery.cs
@@ -1,26 +1,28 @@
-﻿using GraphQL.Types;
+﻿using AutoMapper;
+using GraphQL.Types;
+using HotChocolate;
 using Model;
 using StubLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TarotDB;
 using TarotDB2Model;
 using TarotDTO;
 
 namespace GraphQLApi.Queries
 {
-    public class GameQuery : ObjectGraphType<object>
+    public class GameQuery
     {
-        private const string QUERY_NAME = "GameQuery";
-        private IDataManager _dataManager;
-        public GameQuery(IDataManager dataManager)
+        public async Task<IEnumerable<GameDTO>> GetGame([Service] IDataManager context, [Service] IMapper mapper)
         {
-            this._dataManager = dataManager;
-            Name = QUERY_NAME;
-
+            List<Game> l = new List<Game>(await context.GetGames(0, 10));
+            List<GameDTO> result = new List<GameDTO>();
+            l.ForEach(x => result.Add(mapper.Map<GameDTO>(x)));
+            return result;
         }
 
-
+        public String HelloWorld() => "Hello World !";
     }
 }
