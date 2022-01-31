2828cf334c5021bea24024080dec343f8498a60a
maalbiero <mathieu.albiero@etu.uca.fr>
2022-01-31 12:04:57 +0100

    Added blazor project

---
+++ Sources/APIGateway/Shared/DynamicTable.razor.cs
@@ -0,0 +1,32 @@
+﻿using APIGateway.Model;
+using Blazorise.DataGrid;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APIGateway.Shared
+{
+
+    public partial class DynamicTable
+    {
+        [Parameter]
+        public string NumberElementsPerPage { get; set; }
+        [Parameter]
+        public bool IsEditable { get; set; }
+        [Parameter]
+        public bool IsViewable { get; set; }
+        [Parameter]
+        public bool IsDeletable { get; set; }
+        public List<Data> Elements { get; set; } = new List<Data>();
+        [Parameter]
+        public Func<int, int, Task<List<Data>>> FetchDataWithNumberOfElementsAsync { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var response = await FetchDataWithNumberOfElementsAsync(2, 3);
+            Elements = new List<Data>(response);       
+        }
+
+    }
+}
