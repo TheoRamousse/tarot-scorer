58707de80ee1ba312ca7d0b7b42345ddf5c2b649
thramousse <theo.ramousse@etu.uca.fr>
2022-01-31 15:22:36 +0100

    Created connection with db + started crud

---
+++ Sources/GraphQLApi/Mutation/GameMutation.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TarotDTO;
+
+namespace GraphQLApi.Mutation
+{
+    public class GameMutation
+    {
+        public async Task<GameDTO> AddGame(Game game)
+        {
+            // Omitted code for brevity
+        }
+    }
+}
