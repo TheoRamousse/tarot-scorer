3203e5482e2a02cffbf5de139e178605739cb2a5
thramousse <theo.ramousse@etu.uca.fr>
2022-02-04 17:30:58 +0100

    Finished data deserialize for array

---
+++ Sources/APIGateway/Entity/GameSimplifiedEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace APIGateway
+{
+    public class GameSimplifiedEntity
+    {
+        [JsonPropertyName("GameId")]
+        long Id { get; set; }
+    }
+}
