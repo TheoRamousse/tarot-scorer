bb59bdc1f6219e49b32b357eddacc0988df6b760
maalbiero <mathieu.albiero@etu.uca.fr>
2022-02-10 09:57:33 +0100

    Ajout du UpdatePlayer, DeletePlayer, UpdateGame

---
+++ Sources/BlazorClient/Model/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClient.Model
+{
+    public class Game
+    {
+        public long Id { get; set; }
+
+        public string Date { get; set; }
+
+        public long TakerPoints { get; set; }
+
+        public bool Excuse { get; set; }
+
+        public bool TwentyOne { get; set; }
+
+        public string PetitResult { get; set; }
+
+        public string Poignée { get; set; }
+
+        public string Chelem { get; set; }
+
+        public List<Player> Players { get; set; }
+
+        public string NbPlayers { get; set; }
+
+        public string Rules { get; set; }
+
+    }
+}
