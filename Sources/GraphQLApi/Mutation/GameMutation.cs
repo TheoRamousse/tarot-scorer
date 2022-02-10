aa2633823fc45ab741048d0703bd4d5c2721e905
thramousse <theo.ramousse@etu.uca.fr>
2022-02-10 09:42:56 +0100

    Added errors management in AddPlayer and AddGame

--- Sources/GraphQLApi/Mutation/GameMutation.cs
+++ Sources/GraphQLApi/Mutation/GameMutation.cs
@@ -7,6 +7,7 @@ using HotChocolate.Types;
 using HotChocolate.Types.Relay;
 using Microsoft.Extensions.Logging;
 using Model;
+using System;
 using System.Threading.Tasks;
 using TarotDB;
 using TarotDB2Model;
@@ -17,33 +18,41 @@ namespace GraphQLApi.Mutation
     public class GameMutation
     {
         [GraphQLMetadata("addGame")]
-        public async Task<GameDTOPayload> AddGameAsync(GameDTO gameToAdd, [Service] IDataManager context, [Service] IMapper mapper, [Service] ILogger<GameMutation> logger, [Service] IUnitOfWork unit)
+        public async Task<bool> AddGameAsync(GameDTO gameToAdd, [Service] IDataManager context, [Service] IMapper mapper, [Service] ILogger<GameMutation> logger, [Service] IUnitOfWork unit)
         {
-            var game = mapper.Map<Game>(gameToAdd);
-            game.Rules = new FakeTarotRuleForApi();
-            var result = await context.AddGame(game);
-
-            return null;
+            bool result = true;
+            logger.LogInformation("Entered in the method AddGameAsync with parameters : "+gameToAdd);
+            try
+            {
+                var game = mapper.Map<Game>(gameToAdd);
+                game.Rules = new FakeTarotRuleForApi();
+                await context.AddGame(game);
+            }catch(Exception ex)
+            {
+                result = false;
+            }
+            logger.LogInformation("Exited in the method AddGameAsync");
+            return result;
         }
 
 
         [GraphQLMetadata("addPlayer")]
-        public async Task<PlayerDTOPayload> AddPlayerAsync(PlayerDTO playerToAdd, [Service] IDataManager context, [Service] IMapper mapper, [Service] ILogger<GameMutation> logger)
+        public async Task<bool> AddPlayerAsync(PlayerDTO playerToAdd, [Service] IDataManager context, [Service] IMapper mapper, [Service] ILogger<GameMutation> logger)
         {
-            var player = mapper.Map<Player>(playerToAdd);
-            player.Image = "";
-            var result = await context.AddPlayer(player);
-
-            return null;
+            bool result = true;
+            logger.LogInformation("Entered in the method AddPlayerAsync with parameters : " + playerToAdd);
+            try
+            {
+                var player = mapper.Map<Player>(playerToAdd);
+                player.Image = "";
+                return await context.AddPlayer(player);
+            }
+            catch (Exception ex)
+            {
+                result = false;
+            }
+            return result;
         }
 
-        /*[UseMutationConvention]
-        // [Error(...)] possibilité de prendre en charge des exceptions personnalisées.
-        public async Task<GameDTOPayload> UpdateGameAsync([ID] long gameId, GameDTOInput gameToUpdate, [Service] IDataManager context, [Service] IMapper mapper, [Service] ILogger<GameMutation> logger)
-        {
-            Game gToUp = mapper.Map<Game>(gameToUpdate);
-            var resultat = await context.UpdateGame(gToUp.Id, gToUp);
-            return mapper.Map<GameDTOPayload>(resultat);
-        }*/
     }
 }
