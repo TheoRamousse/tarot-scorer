11bf077a14b18ca9456dbe191833e89cc3ade59c
rahacques <raphael.hacques@etu.uca.fr>
2022-02-11 17:41:40 +0100

    merged Game Extension

--- Sources/TarotDTO/GameDTO.cs
+++ Sources/TarotDTO/GameDTO.cs
@@ -8,7 +8,7 @@ namespace TarotDTO
 {
     public class GameDTO : IDTO
     {
-        public long Id { get; private set; } = 0;
+        public long Id { get; set; } = 0;
 
         public DateTime Date { get;  set; }
 
